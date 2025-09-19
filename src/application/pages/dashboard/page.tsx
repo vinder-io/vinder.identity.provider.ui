ccc9335b09fe5671f1166e3f3023ca2d8cb0d851
Richard Garcia <code.richardy@gmail.com>
2025-09-19 19:57:06 -0300

    introduces dashboard-page.tsx to render the main dashboard within the app's primary layout

---
+++ src/application/pages/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { MainLayout } from "@/application/components/shared/main-layout";
+
+export default function DashboardPage() {
+    return (
+        <MainLayout>
+            <span></span>
+        </MainLayout>
+    );
+}
