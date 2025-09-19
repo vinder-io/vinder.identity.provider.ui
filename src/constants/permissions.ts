33295328c8a475e5e4d39ba503ac9a80c03a474f
Richard Garcia <code.richardy@gmail.com>
2025-09-19 20:01:46 -0300

    introduces permissions class to centralize all application permission constants

---
+++ src/constants/permissions.ts
@@ -0,0 +1,22 @@
+export class Permissions {
+    public static readonly CreateGroup = "vinder.defaults.permissions.group.create";
+    public static readonly DeleteGroup = "vinder.defaults.permissions.group.delete";
+    public static readonly EditGroup = "vinder.defaults.permissions.group.update";
+    public static readonly ViewGroups = "vinder.defaults.permissions.group.view";
+
+    public static readonly DeleteUser = "vinder.defaults.permissions.user.delete";
+    public static readonly EditUser = "vinder.defaults.permissions.user.update";
+    public static readonly ViewUsers = "vinder.defaults.permissions.user.view";
+
+    public static readonly CreatePermission = "vinder.defaults.permissions.permissions.create";
+    public static readonly AssignPermissions = "vinder.defaults.permissions.permissions.assign";
+    public static readonly RevokePermissions = "vinder.defaults.permissions.permissions.revoke";
+    public static readonly ViewPermissions = "vinder.defaults.permissions.permissions.view";
+    public static readonly EditPermission = "vinder.defaults.permissions.permissions.edit";
+    public static readonly DeletePermission = "vinder.defaults.permissions.permissions.delete";
+
+    public static readonly CreateTenant = "vinder.defaults.permissions.tenant.create";
+    public static readonly DeleteTenant = "vinder.defaults.permissions.tenant.delete";
+    public static readonly EditTenant = "vinder.defaults.permissions.tenant.update";
+    public static readonly ViewTenants = "vinder.defaults.permissions.tenant.view";
+}
