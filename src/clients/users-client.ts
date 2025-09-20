8eb8f90d9fb6ed19fdf982a714f773c90d546c28
Richard Garcia <code.richardy@gmail.com>
2025-09-19 23:06:40 -0300

    introduces get users method in UsersClient, enabling fetching paginated user data with optional filters and tenant specification, using QueryParametersParser for query string serialization

--- src/clients/users-client.ts
+++ src/clients/users-client.ts
@@ -1,10 +1,38 @@
-import type { GroupBasicDetails } from "@/types/group/group-basic-details";
-
 import { Result } from "@/types/common/result";
 import { Storage } from "@/utils/storage";
 import { StorageKeys } from "@/constants/storage-keys";
+import { QueryParametersParser } from "@/utils/query-parameters-parser";
+
+import type { GroupBasicDetails } from "@/types/group/group-basic-details";
+import type { UsersFetchParameters } from "@/types/user/users-fetch-parameters";
+import type { Pagination } from "@/types/common/pagination";
+import type { UserDetails } from "@/types/identity/user-details";
 
 export class UsersClient {
+    public static async getUsers(tenant: string = "master", parameters?: UsersFetchParameters): Promise<Result<Pagination<UserDetails>>> {
+        const filters: UsersFetchParameters = { pageNumber: 1, pageSize: 20, ...parameters };
+        const token = Storage.getItem(StorageKeys.AccessToken);
+
+        const queryString = QueryParametersParser.toQueryString(filters);
+        const url = `http://vinder-io-identity-provider.somee.com/api/v1/users?${queryString}`;
+
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+                "X-Tenant": tenant,
+            },
+        });
+
+        const content = await response.json();
+        if (!response.ok) {
+            return Result.failure<Pagination<UserDetails>>(content);
+        }
+
+        return Result.success<Pagination<UserDetails>>(content);
+    }
+
     public static async getUserGroups(id: string): Promise<Result<GroupBasicDetails>> {
         const token = Storage.getItem(StorageKeys.AccessToken);
         const httpMessage = {
