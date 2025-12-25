95dd6200fdc38c1bcabf9193c807794c29accb54
Richard Garcia <code.richardy@gmail.com>
2025-12-25 17:06:00 -0300

    feat: add usePermissions hook for fetching permissions data

---
+++ src/application/pages/permissions/hooks/use-permissions.ts
@@ -0,0 +1,14 @@
+import { PermissionsClient } from "@/clients/permissions-client";
+import { useQuery } from "@tanstack/react-query";
+
+import type { PermissionsFetchParameters } from "@/types/permissions/permissions-fetch-parameters";
+import type { Result } from "@/types/common/result";
+import type { Pagination } from "@/types/common/pagination";
+import type { Permission } from "@/types/identity/permission";
+
+export function usePermissions(parameters: PermissionsFetchParameters = {  }) {
+  return useQuery<Result<Pagination<Permission>>>({
+    queryKey: ["permissions", parameters],
+    queryFn: async () => PermissionsClient.getPermissionsAsync(parameters),
+  });
+}
