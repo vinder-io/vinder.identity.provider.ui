80dce1ab41f2ed248061afd0d32ae11e0a84c1e1
Richard Garcia <code.richardy@gmail.com>
2025-12-25 17:05:18 -0300

    refactor: wrap application in QueryClientProvider for react-query integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client"
 import { StrictMode } from "react"
 import { Toaster } from "react-hot-toast"
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/query-client";
 
 import "./index.css"
 import AppRoutes from "./routes/routes.tsx"
@@ -10,11 +12,13 @@ import { AuthorizationStateProvider } from "./contexts/authorization-context.tsx
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <AuthenticationStateProvider>
-            <AuthorizationStateProvider>
-                <Toaster position="bottom-right" />
-                <AppRoutes />
-            </AuthorizationStateProvider>
-        </AuthenticationStateProvider>
+        <QueryClientProvider client={queryClient}>
+            <AuthenticationStateProvider>
+                <AuthorizationStateProvider>
+                    <Toaster position="bottom-right" />
+                    <AppRoutes />
+                </AuthorizationStateProvider>
+            </AuthenticationStateProvider>
+        </QueryClientProvider>
     </StrictMode>
 )
